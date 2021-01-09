1dca5b0a4ed21173b13c0d655a48272803212d70
lucyyxc <xilucychen@gmail.com>
2021-01-08 18:54:28 -0800

    challenge 2 completed

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -2,11 +2,31 @@ import React, { useEffect, useState } from 'react';
 import BTCChart from './BTCChart';
 
 const App = () => {
-  // historical data API: https://api.coindesk.com/v1/bpi/historical/close.json
+  const API_URL = 'https://api.coindesk.com/v1/bpi/historical/close.json';
+
+  const [dates, setDates] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = () => {
+    fetch(API_URL)
+      .then((response) => {
+        // console.log(response.json());
+        response.json().then((data) => {
+          // console.log(data);
+          setDates(Object.keys(data.bpi));
+          setData(Object.values(data.bpi));
+        });
+      })
+      .catch((error) => console.log('Fetch error:', error));
+  };
 
   return (
     <div>
-      <BTCChart />
+      <BTCChart dates={dates} data={data} />
       <a href='https://www.coindesk.com/price/bitcoin' target='_blank'>
         Powered by CoinDesk
       </a>
